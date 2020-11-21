b454a5dee1efb6194a0a0e5431a54e53ea889e0a
Josh Sanderlin <josh.sanderlin@elasticsuite.com>
2020-11-21 15:00:14 -0700

    add frontend build

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
-
 const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+
+app.use(express.static('build'))
 
 const requestLogger = (request, response, next) => {
   console.log('Method: ', request.method)
@@ -10,8 +14,6 @@ const requestLogger = (request, response, next) => {
   console.log('---')
   next()
 }
-app.use(cors())
-app.use(express.json())
 app.use(requestLogger)
 
 let notes = [
