59efae5557951d979fc71bd4cdf2ad221ca8ef19
Josh Sanderlin <josh.sanderlin@elasticsuite.com>
2020-11-22 16:26:25 -0700

    Finished Part 3c

---
+++ models/note.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+/* Configure MongoDB connection */
+const db_password = process.env.DB_PASSWORD
+const db_username = process.env.DB_USERNAME
+const db_collection = process.env.DB_COLLECTION
+const db_url =
+  `mongodb+srv://${db_username}:${db_password}@cluster0.y42xl.mongodb.net/${db_collection}?retryWrites=true&w=majority`
+
+console.log('connecting to MongoDB...')
+
+mongoose.connect(db_url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(result => {
+    console.log('Connection to MongoDB established!')
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB:', error.message)
+  })
+
+/* Configure Schema */
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
+
---
+++ mongo.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+
+if(process.argv.length < 2) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const db_password = process.env.DB_PASSWORD
+const db_username = process.env.DB_USERNAME
+const db_collection = process.env.DB_COLLECTION
+
+const db_url =
+  `mongodb+srv://${db_username}:${db_password}@cluster0.y42xl.mongodb.net/${db_collection}?retryWrites=true&w=majority`
+
+mongoose.connect(db_url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true})
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
+
+// const note = new Note({
+//   content: 'Cyberpunk 2077 is gonna be the tits',
+//   date: new Date(),
+//   important: true,
+// })
+
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
